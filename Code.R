3c43b381f63c96793af2332c02759df5f7a18f37
Kian Wee <leekianwee@outlook.com>
2019-10-28 13:29:08 +0800

    Added R code

---
+++ Code.R
@@ -0,0 +1,32 @@
+# Requried Lib
+library(tidyverse)
+library(gganimate)
+library(ggplot2)
+library(plyr)
+library(countrycode)
+
+# Importing datasets
+data1 <- read.csv("/Users/kianweelee/Desktop/rstat/Life_Expectancy_Data.txt", header = T, sep = "\t")
+
+# Adding a new column containing continent 
+data1$Continent <- countrycode(sourcevar = data1[, "Country"],
+                               origin = "country.name",
+                               destination = "continent")
+
+# Plotting static graph
+x = ggplot(data1, aes(log(GDP), Life.expectancy, col = Continent, size = Population)) + 
+  geom_point(alpha = 0.5) +
+  facet_wrap(~ Continent) +
+  geom_smooth(method = lm)
+theme_linedraw() +
+  ggtitle("Effect of GDP on life expectancy")
+
+# Running linear regression model, with response variable being life exp and explanatory variable being GDP  
+summary(lm(data1$Life.expectancy~data1$GDP))
+
+# Implementing gganimate 
+anim <- x + transition_time(data1$Year) +
+  labs(title = "Year: {frame_time}") +
+  ease_aes('linear')
+
+anim_save("anim1.gif", anim)
